0e041a4ce9c7924602d1358f94351fc4e2c0921b
vaibhavi <vaibhavinandedkar@gmail.com>
2026-07-02 18:12:14 +0530

    SMS-014: Add GET all students API

--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -1,23 +1,32 @@
 from flask import Blueprint, jsonify
 
-student_bp = Blueprint(
-    "students",
-    __name__
-)
+from app.services.student_service import StudentService
+
+student_bp = Blueprint("students", __name__)
+
+student_service = StudentService()
 
 
-@student_bp.route(
-    "/",
-    methods=["GET"]
-)
+@student_bp.route("/", methods=["GET"])
 def get_students():
     """
-    Get all students.
+    Get all students
     """
 
-    return jsonify(
-        {
-            "message": "Student API Working",
-            "status": "success"
-        }
-    )
+    students = student_service.get_all_students()
+
+    result = []
+
+    for student in students:
+        result.append({
+            "id": student.id,
+            "student_code": student.student_code,
+            "first_name": student.first_name,
+            "last_name": student.last_name,
+            "email": student.email,
+            "department": student.department,
+            "semester": student.semester,
+            "status": student.status
+        })
+
+    return jsonify(result), 200
